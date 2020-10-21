11831062745d09143784a3ab6f957c2166fe9237
apurvak27 <73236127+apurvak27@users.noreply.github.com>
2020-10-21 14:51:07 +0100

    Update level3.py

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -3,5 +3,5 @@
 def oddish_evenish_num(n):
 	return 'Oddish' if sum(map(int, str(n))) % 2 else 'Evenish'
 
-	
-print(oddish_evenish_num(1190))
+if __name__ == '__main__':	
+ print(oddish_evenish_num(1190))
