04fc64e4c09077eb6e112b19f75eb8180a402472
James Bradford <40271160+MarbleToast@users.noreply.github.com>
2020-10-21 14:52:43 +0100

    Update level3.py

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -1,7 +1,21 @@
 """ Hackathon - Level 3 """
 
 def oddish_evenish_num(n):
-	return 'Oddish' if sum(map(int, str(n))) % 2 else 'Evenish'
+    """
+    Returns Oddish if odd, else returns Evenish
+    
 
-	
-print(oddish_evenish_num(1190))
+    Parameters
+    ----------
+    n : integer to evaluate
+
+    Returns
+    -------
+    Oddish: if n == odd
+    Evenish: if n == even
+
+    """
+    return 'Oddish' if sum(map(int, str(n))) % 2 else 'Evenish'
+
+if __name__ == "__main__" :
+    print(oddish_evenish_num(1190))
