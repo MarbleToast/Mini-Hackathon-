efe8b6b1001e782115be91dca9bb3d6389b9c38d
James Bradford <40271160+MarbleToast@users.noreply.github.com>
2020-10-21 18:21:46 +0100

    Update beautiful-poetry.py

--- Creative-Challenges/beautiful-poetry.py
+++ Creative-Challenges/beautiful-poetry.py
@@ -4,20 +4,42 @@ Created on Wed Oct 21 15:09:03 2020
 
 @author: jrbra
 """
-import re
+import pyphen
+dic = pyphen.Pyphen(lang='en')
+
 
 def read_word_list():
     with open("word_list.txt", "r") as f  :
         return [i.strip() for i in f.readlines()]
     
+    
 def syllable_count(word):
-    regex = r"([^aeiouy])(a|e|i|o|u|y)"
-    
-    matches = re.findall(regex, word, re.MULTILINE)
-    
-    count = len(matches)
-    if ['a', 'e', 'i', 'o', 'u'].__contains__(word[0]):
-        count+=1
-    return count
+    return len(dic.inserted(word).split("-"))
 
-print(syllable_count("What"))
+
+def parse_syllables():
+    syl_dict = {}
+    for word in read_word_list():
+        syl_count = syllable_count(word)
+        if not syl_count in syl_dict.keys():
+            syl_dict[syl_count] = [word]
+        else:
+            syl_dict[syl_count].append(word)
+    return syl_dict
+
+
+def subset_sum(numbers, target, partial=[]):
+    s = sum(partial)
+
+    # check if the partial sum is equals to target
+    if s == target:
+        print("sum(%s)=%s" % (partial, target))
+    if s >= target:
+        return  # if we reach the number why bother to continue
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        subset_sum(numbers, target, partial + [n])
+
+syl_dictionary = parse_syllables()
+print(subset_sum(list(syl_dictionary.keys()), 2))
